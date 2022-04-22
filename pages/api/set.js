9b4fc69b559b5b2a1875b20c3a7d13e3a720e022
Adhy Wiranata <adhy.prasetyo@tiket.com>
2022-04-22 21:09:22 +0700

    upp

--- pages/api/set.js
+++ pages/api/set.js
@@ -1,9 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  const newToken = "token=token__" + Math.random() * 100;
   res.setHeader(
     "Set-Cookie",
-    "token=token__" + Math.random() * 100 + "; HttpOnly;"
+    newToken + "; HttpOnly; expires=Thu, Jan 20 2025 00:00:00 UTC;"
   );
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ newToken });
 }
--- pages/api/unset.js
+++ pages/api/unset.js
@@ -1,11 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  const newToken = "token=token__" + Math.random() * 100;
   res.setHeader(
     "Set-Cookie",
-    "token=token__" +
-      Math.random() * 100 +
-      "; HttpOnly; expires=Thu, Jan 01 1970 00:00:00 UTC;"
+    newToken + "; HttpOnly; expires=Thu, Jan 01 1970 00:00:00 UTC;"
   );
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ newToken });
 }
