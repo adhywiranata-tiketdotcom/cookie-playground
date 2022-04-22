d401db6f03be0c50461c108578fb55309689fdaa
Adhy Wiranata <adhy.prasetyo@tiket.com>
2022-04-22 20:57:07 +0700

    up

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.setHeader(
+    "Set-Cookie",
+    "token=token__" + Math.random() * 100 + "; HttpOnly;"
+  );
+  res.status(200).json({ name: "John Doe" });
 }
