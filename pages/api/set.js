da6b3792946d99a93969fa95491183edd8376631
Adhy Wiranata <adhy.prasetyo@tiket.com>
2022-04-26 08:38:02 +0700

    update

--- pages/api/set.js
+++ pages/api/set.js
@@ -2,10 +2,16 @@
 
 export default function handler(req, res) {
   const newToken = "token=token__" + Math.random() * 100;
+  const newToken2 = "token=token__" + Math.random() * 100;
   res.setHeader(
     "Set-Cookie",
     newToken +
       "; HttpOnly; Expires=Mon, Jun 13 2022 00:00:00 UTC; Path=/; Domain=.cookie-playground.vercel.app;"
   );
-  res.status(200).json({ newToken });
+  res.setHeader(
+    "Set-Cookie",
+    newToken2 +
+      "; HttpOnly; Expires=Mon, Jun 13 2022 00:00:00 UTC; Path=/; Domain=.cookie-playground.vercel.app;"
+  );
+  res.status(200).json({ newToken, newToken2 });
 }
--- pages/api/unset.js
+++ pages/api/unset.js
@@ -2,10 +2,17 @@
 
 export default function handler(req, res) {
   const newToken = "token=token__" + Math.random() * 100;
+  const newToken2 = "token=token__" + Math.random() * 100;
+
   res.setHeader(
     "Set-Cookie",
     newToken +
       "; HttpOnly; Expires=Thu, Jan 01 1970 00:00:00 UTC; Path=/; Domain=.cookie-playground.vercel.app;"
   );
-  res.status(200).json({ newToken });
+  res.setHeader(
+    "Set-Cookie",
+    newToken2 +
+      "; HttpOnly; Expires=Thu, Jan 01 1970 00:00:00 UTC; Path=/; Domain=.cookie-playground.vercel.app;"
+  );
+  res.status(200).json({ newToken, newToken2 });
 }
